1e89816a49542f71fa00b7a48fccd46318b19d1c
Stein Gunnar Grastveit <grastvei@pvv.org>
2020-08-05 17:21:22 +0200

    Update menu with log view action

--- gui/menubar.h
+++ gui/menubar.h
@@ -1,12 +1,26 @@
 #ifndef MENUBAR_H
 #define MENUBAR_H
 
+#include <memory>
+
 #include <QMenuBar>
+#include <QMenu>
+#include <QAction>
 
 class Menubar : public QMenuBar
 {
+    Q_OBJECT
 public:
     Menubar();
+
+signals:
+    void logViewTriggered(bool);
+
+private:
+    std::unique_ptr<QMenu> mViewMenu;
+    std::unique_ptr<QMenu> mFileMenu;
+
+    std::unique_ptr<QAction> mLogViewAction;
 };
 
 #endif // MENUBAR_H
