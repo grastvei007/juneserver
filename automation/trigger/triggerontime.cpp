3ecea0abe4259ec1539bd3b323f920d09a0ef4b3
Stein Gunnar Grastveit <grastvei@pvv.org>
2025-10-23 11:15:15 +0200

    Bugfix: allow booth on and off

--- automation/trigger/triggerontime.cpp
+++ automation/trigger/triggerontime.cpp
@@ -79,7 +79,7 @@ void TriggerOnTime::tagSocketValueChanged(TagSocket *tagSocket)
 void TriggerOnTime::resetState()
 {
     hasTriggeredOn_ = false;
-    hasTriggeredOff_ = true;
+    hasTriggeredOff_ = false;
     if(isActive())
         setDeactive();
 }
